c5950259d517b0a0984c8bed9d963e5b61544e96
Kenneth Paulson <Kenneth Paulson>
2016-08-12 09:08:01 -0400

    up date to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
-                <div>Hi!</div>
+                <div>Hi!!</div>
             </div>
         </div>
     </body>
