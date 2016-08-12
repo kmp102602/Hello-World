032cac6d635f860d337ed97ff45d47cd630104ad
Kenneth Paulson <Kenneth Paulson>
2016-08-12 08:37:40 -0400

    added chages to welcome message.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <div>Hi!</div>
             </div>
         </div>
     </body>
